e49c721557ffdf2477d92a53a3af4767f609f148
zhangxing1990 <294302153@qq.com>
2015-01-24 18:44:56 +0800

    测试修改

--- smartmq-store/src/main/java/Test.java
+++ smartmq-store/src/main/java/Test.java
@@ -1,10 +1,11 @@
-
 /**
  * TODO
- *
+ * 
  * @author 张星
- * @date  2015年1月24日
+ * @date 2015年1月24日
  */
 public class Test {
-
+    public static void main(String[] args) {
+        System.out.println(111222);
+    }
 }
